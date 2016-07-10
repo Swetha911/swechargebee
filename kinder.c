8e3413cc1c6c3f6c1c4c19a7287780d081e3a358
Swetha911 <Swetha911@users.noreply.github.com>
2016-07-10 17:34:18 +0530

    Update kinder.c

--- kinder.c
+++ kinder.c
@@ -2,15 +2,15 @@
 #include<conio.h>
 int main()
 {
-int a,b,c,i,j=1;
+int a,x,c,i,j=1;
 printf("Enter the n values:");
 scanf("%d",&a);
 printf("Enter the pair:");
 scanf("%d",&c);
 for(i=0;i<2*c;i++)
 {
-b=2*a-i;
-if(c!=b)
+x=2*a-i;
+if(c!=x)
 {
 printf("The pair is %d\n",c,j++,c);
 }
